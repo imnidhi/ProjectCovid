7217d97278e560636280e1e370bbd869161b7d71
imnidhi <nidhim214@gmail.com>
2020-05-01 18:16:05 +0530

    Added source

--- lib/main.dart
+++ lib/main.dart
@@ -95,141 +95,170 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Scaffold(
               endDrawer: Drawer(
                 child: Column(
-                  children: <Widget>[
-                    DrawerHeader(
-                      child: Align(
-                          alignment: Alignment.bottomLeft,
-                          child: Text(
-                            'Useful Links',
-                            style: TextStyle(
-                                color: Colors.redAccent, fontSize: 20),
-                          )),
-                      decoration: BoxDecoration(
-                        color: Colors.black,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top:8.0,left:10,right:10),
-                      child: new InkWell(
-                          child: FittedBox(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: <Widget>[
+                        DrawerHeader(
+                          child: Align(
+                              alignment: Alignment.bottomLeft,
+                              child: Text(
+                                'Useful Links',
+                                style: TextStyle(
+                                    color: Colors.redAccent, fontSize: 20),
+                              )),
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              top: 8.0, left: 10, right: 10),
+                          child: new InkWell(
+                              child: FittedBox(
+                                  child: new Text(
+                                'WORLD HEALTH ORGANISATION',
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.blue,
+                                    decoration: TextDecoration.underline),
+                              )),
+                              onTap: () async {
+                                String url =
+                                    "https://www.who.int/emergencies/diseases/novel-coronavirus-2019";
+                                if (await canLaunch(url)) {
+                                  await launch(url);
+                                } else {
+                                  throw 'Could not launch $url';
+                                }
+                              }),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 40.0),
+                          child: new InkWell(
                               child: new Text(
-                            'WORLD HEALTH ORGANISATION',
-                            style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.blue,
-                                decoration: TextDecoration.underline),
-                          )),
-                          onTap: () async {
-                            String url =
-                                "https://www.who.int/emergencies/diseases/novel-coronavirus-2019";
-                            if (await canLaunch(url)) {
-                              await launch(url);
-                            } else {
-                              throw 'Could not launch $url';
-                            }
-                          }),
+                                'CDC',
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.blue,
+                                    decoration: TextDecoration.underline),
+                              ),
+                              onTap: () async {
+                                String url =
+                                    "https://www.cdc.gov/coronavirus/2019-ncov/index.html";
+                                if (await canLaunch(url)) {
+                                  await launch(url);
+                                } else {
+                                  throw 'Could not launch $url';
+                                }
+                              }),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 40.0),
+                          child: new InkWell(
+                              child: new Text(
+                                'NHS',
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.blue,
+                                    decoration: TextDecoration.underline),
+                              ),
+                              onTap: () async {
+                                String url =
+                                    "https://www.nhs.uk/conditions/coronavirus-covid-19/";
+                                if (await canLaunch(url)) {
+                                  await launch(url);
+                                } else {
+                                  throw 'Could not launch $url';
+                                }
+                              }),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 40.0),
+                          child: new InkWell(
+                              child: new Text(
+                                'GOOGLE NEWS',
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.blue,
+                                    decoration: TextDecoration.underline),
+                              ),
+                              onTap: () async {
+                                String url =
+                                    "https://news.google.com/topstories?hl=en-IN&gl=IN&ceid=IN:en";
+                                if (await canLaunch(url)) {
+                                  await launch(url);
+                                } else {
+                                  throw 'Could not launch $url';
+                                }
+                              }),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              top: 40.0, right: 10, left: 10),
+                          child: new InkWell(
+                              child: FittedBox(
+                                child: new Text(
+                                  'GOOGLE INFO and RESOURCES',
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      color: Colors.blue,
+                                      decoration: TextDecoration.underline),
+                                ),
+                              ),
+                              onTap: () async {
+                                String url =
+                                    "https://www.google.com/intl/en_in/covid19/";
+                                if (await canLaunch(url)) {
+                                  await launch(url);
+                                } else {
+                                  throw 'Could not launch $url';
+                                }
+                              }),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 40.0),
+                          child: new InkWell(
+                              child: new Text(
+                                'MoHFW India',
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.blue,
+                                    decoration: TextDecoration.underline),
+                              ),
+                              onTap: () async {
+                                String url = "https://www.mohfw.gov.in/#";
+                                if (await canLaunch(url)) {
+                                  await launch(url);
+                                } else {
+                                  throw 'Could not launch $url';
+                                }
+                              }),
+                        ),
+                      ],
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(top:40.0),
-                      child: new InkWell(
-                          child: new Text(
-                            'CDC',
-                            style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.blue,
-                                decoration: TextDecoration.underline),
-                          ),
-                          onTap: () async {
-                            String url =
-                                "https://www.cdc.gov/coronavirus/2019-ncov/index.html";
-                            if (await canLaunch(url)) {
-                              await launch(url);
-                            } else {
-                              throw 'Could not launch $url';
-                            }
-                          }),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top:40.0),
-                      child: new InkWell(
-                          child: new Text(
-                            'NHS',
-                            style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.blue,
-                                decoration: TextDecoration.underline),
-                          ),
-                          onTap: () async {
-                            String url =
-                                "https://www.nhs.uk/conditions/coronavirus-covid-19/";
-                            if (await canLaunch(url)) {
-                              await launch(url);
-                            } else {
-                              throw 'Could not launch $url';
-                            }
-                          }),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top:40.0),
-                      child: new InkWell(
-                          child: new Text(
-                            'GOOGLE NEWS',
-                            style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.blue,
-                                decoration: TextDecoration.underline),
-                          ),
-                          onTap: () async {
-                            String url =
-                                "https://news.google.com/topstories?hl=en-IN&gl=IN&ceid=IN:en";
-                            if (await canLaunch(url)) {
-                              await launch(url);
-                            } else {
-                              throw 'Could not launch $url';
-                            }
-                          }),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top:40.0,right:10,left:10),
-                      child: new InkWell(
-                          child: FittedBox(
-                            child: new Text(
-                              'GOOGLE INFO and RESOURCES',
+                    SizedBox(
+                      width: double.infinity,
+                      child: Container(
+                        color: Colors.grey[600],
+                        child: Column(
+                          children: [
+                            Text(
+                              "Sources",
                               style: TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.blue,
-                                  decoration: TextDecoration.underline),
+                                fontSize: 14,
+                                color: Colors.white,
+                              ),
                             ),
-                          ),
-                          onTap: () async {
-                            String url =
-                                "https://www.google.com/intl/en_in/covid19/";
-                            if (await canLaunch(url)) {
-                              await launch(url);
-                            } else {
-                              throw 'Could not launch $url';
-                            }
-                          }),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top:40.0),
-                      child: new InkWell(
-                          child: new Text(
-                            'MoHFW India',
-                            style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.blue,
-                                decoration: TextDecoration.underline),
-                          ),
-                          onTap: () async {
-                            String url = "https://www.mohfw.gov.in/#";
-                            if (await canLaunch(url)) {
-                              await launch(url);
-                            } else {
-                              throw 'Could not launch $url';
-                            }
-                          }),
-                    ),
+                            Text("Postman COVID-19 API Resource Center",
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.white,
+                                )),
+                          ],
+                        ),
+                      ),
+                    )
                   ],
                 ),
               ),
